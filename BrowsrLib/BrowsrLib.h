54aeeb6c4b87e7f7a3aac87f748682efa466f4cc
Sunil Zishan <sunilzishan85@gmail.com>
2023-07-24 14:32:39 +0200

    Initial Commit

---
+++ BrowsrLib/BrowsrLib.h
@@ -0,0 +1,18 @@
+//
+//  BrowsrLib.h
+//  BrowsrLib
+//
+//  Created by Sunil Zishan on 24.07.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrowsrLib.
+FOUNDATION_EXPORT double BrowsrLibVersionNumber;
+
+//! Project version string for BrowsrLib.
+FOUNDATION_EXPORT const unsigned char BrowsrLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrowsrLib/PublicHeader.h>
+
+
